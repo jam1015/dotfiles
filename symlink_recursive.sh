aaf57bcbf95ba78f37a8353fbd35c093119187ea
jam1015 <jordan.mandel@live.com>
2021-12-21 22:25:20 -0500

    made some more changes

--- symlink_recursive.sh
+++ symlink_recursive.sh
@@ -12,38 +12,42 @@ recursive_ln()
 	echo ====================================================
 	echo "trying to link $1 and $2"
 
-	if    [ -d $1 ] && [ -f $2 ]
+	if    [ -d "$1" ] && [ -f "$2" ]
 	then
 		echo "type mismatch between $1 (directory) and $2 (file)"
 		return 0
 	fi
 
-	if  [ -f $1 ] && [ -d $2 ]
+	if  [ -f "$1" ] && [ -d "$2" ]
 	then
 		echo "type mismatchbetween $1 (file) and $2 (directory)"
 		return 0
 	fi
 
 	# seeing whether they are both files
-	if [ -f $1 ] && [ -f $2 ]
+	if [ -f "$1" ] && [ -f "$2" ]
 	then
 		echo "both files, not symlinking"
 	fi
 
 	# checking if the file already exists
 
-	if [ -d $1 ] && [ -d $2 ]
+	if [ -d "$1" ] && [ -d "$2" ]
 	then
 		echo "both directories: recursively symlinking contents"
-		for fle in "$1/*"
+		for fle in "$1"/.*
 		do
+			echo "file is $fle"
 			fle_used="$(basename -- $fle)"
-			if [ "$fle_used" != '..' ] && [ "$fle_used" != '.' ] && [ "$fle_used" != 'symlink_recursive.sh' ] && [ fle_used!="dotfiles" ]
+			echo "basename is $fle_used"
+			if [ "$fle_used" != '..' ] && [ "$fle_used" != '.' ] && [ "$fle_used" != 'symlink_recursive.sh' ] && [ fle_used!="dotfiles" ] && [ fle_used!="README.md" ]
 			then
+				echo "applying recursion"
 				recursive_ln "$1/$fle_used" "$2/$fle_used"
+			else
+				echo "file rejected for recursion"
 			fi
 		done
-		return 0
 	fi
 
 	echo "linking $1 to $2"
