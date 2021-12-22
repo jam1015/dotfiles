7cbf5e843b938ab23571767a8e2b5f088d8d14c5
jam1015 <jordan.mandel@live.com>
2021-12-21 21:37:45 -0500

    added recursive symlink script

---
+++ symlink_recursive.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# script meant to be kept in my dotfiles to recursively link the files to the home directory
+# clone the dotfiles repo to ~
+# then run this script to recursively copy all of the files
+# TODO use basename in declaration of loop rather than inside the loop
+# look up `--` syntax used with basename
+
+recursive_ln()
+{
+	# these two conditions test if one is a file and the other is a directory or vice-versa
+	echo ====================================================
+	echo "trying to link $1 and $2"
+
+	if    [ -d $1 ] && [ -f $2 ]
+	then
+		echo "type mismatch between $1 (directory) and $2 (file)"
+		return 0
+	fi
+
+	if  [ -f $1 ] && [ -d $2 ]
+	then
+		echo "type mismatchbetween $1 (file) and $2 (directory)"
+		return 0
+	fi
+
+	# seeing whether they are both files
+	if [ -f $1 ] && [ -f $2 ]
+	then
+		echo "both files, not symlinking"
+	fi
+
+	# checking if the file already exists
+
+	if [ -d $1 ] && [ -d $2 ]
+	then
+		echo "both directories: recursively symlinking contents"
+		for fle in "$1/*"
+		do
+			fle_used="$(basename -- $fle)"
+			if [ "$fle_used" != '..' ] && [ "$fle_used" != '.' ] && [ "$fle_used" != 'symlink_recursive.sh' ] && [ fle_used!="dotfiles" ]
+			then
+				base=$(basename "$fle")
+				recursive_ln "$1/$fle" "$2/$base"
+			fi
+		done
+		return 0
+	fi
+
+	echo "linking $1 to $2"
+	#ln -sv $1 $2
+	return 0
+}
+
+recursive_ln ~/dotfiles ~
