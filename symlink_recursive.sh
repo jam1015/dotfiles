e77501e7d5ccc14baadfe5c7879e22a0e7153186
jam1015 <jordan.mandel@live.com>
2021-12-21 23:28:41 -0500

    made modification to recursion script

--- symlink_recursive.sh
+++ symlink_recursive.sh
@@ -5,54 +5,69 @@
 # then run this script to recursively copy all of the files
 # TODO use basename in declaration of loop rather than inside the loop
 # look up `--` syntax used with basename
+echo_indent()
+{
+	i=0
+	while [ $i -lt $1 ]
+	do
+		echo -n "  "
+		i=$((i+1))
+	done
+	echo "$2"
+}
 
 recursive_ln()
 {
+	n_indents=$3
 	# these two conditions test if one is a file and the other is a directory or vice-versa
-	echo ====================================================
-	echo "trying to link $1 and $2"
+	echo_indent $3 ====================================================
+	echo_indent $3 "trying to link $1 and $2"
 
 	if    [ -d "$1" ] && [ -f "$2" ]
 	then
-		echo "type mismatch between $1 (directory) and $2 (file)"
-		return 0
+		echo_indent $3 "type mismatch between $1 (directory) and $2 (file)"
+		return 1
 	fi
 
 	if  [ -f "$1" ] && [ -d "$2" ]
 	then
-		echo "type mismatchbetween $1 (file) and $2 (directory)"
-		return 0
+		echo_indent $3 "type mismatchbetween $1 (file) and $2 (directory)"
+		return 1
 	fi
 
 	# seeing whether they are both files
 	if [ -f "$1" ] && [ -f "$2" ]
 	then
-		echo "both files, not symlinking"
+		echo_indent $3 "both files, not symlinking"
+		return 1
 	fi
 
 	# checking if the file already exists
 
 	if [ -d "$1" ] && [ -d "$2" ]
 	then
-		echo "both directories: recursively symlinking contents"
+		echo_indent $3 "both directories: recursively symlinking contents"
 		for fle in "$1"/.*
 		do
-			echo "file is $fle"
+			echo_indent $3 '------------------------------------------'
+			echo_indent $3 "file is $fle"
 			fle_used="$(basename -- $fle)"
-			echo "basename is $fle_used"
+			echo_indent $3 "basename is $fle_used"
 			if [ "$fle_used" != '..' ] && [ "$fle_used" != '.' ] && [ "$fle_used" != 'symlink_recursive.sh' ] && [ fle_used!="dotfiles" ] && [ fle_used!="README.md" ]
 			then
-				echo "applying recursion"
-				recursive_ln "$1/$fle_used" "$2/$fle_used"
+				echo_indent $3 "applying recursion"
+				n_indents=$((n_indents+1))
+				recursive_ln "$1/$fle_used" "$2/$fle_used" $((n_indents))
+				n_indents=$((n_indents-1))
 			else
-				echo "file rejected for recursion"
+				echo_indent $3 "file rejected for recursion"
 			fi
 		done
 	fi
 
-	echo "linking $1 to $2"
+	echo_indent $n_indents "linking $1 to $2"
 	#ln -sv $1 $2
 	return 0
 }
 
-recursive_ln ~/dotfiles ~
+recursive_ln ~/dotfiles ~ 0
