2cadad88c9a838b4f1d52448adf1fe0b55021878
Jordan Mandel <jordan.mandel@live.com>
2022-03-15 14:55:33 -0400

    here are some changes

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -2,7 +2,10 @@
            (env . (("DOOMDIR" . "~/.doom.d")))))
 
 ("default" . ((user-emacs-directory . "~/.emacs.default")))
+
 ("evil" . ((user-emacs-directory . "~/.emacs.evil")))
 
+;; ("spacemacs" . ((user-emacs-directory . "~/spacemacs")
+;;                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
  ("spacemacs" . ((user-emacs-directory . "~/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
