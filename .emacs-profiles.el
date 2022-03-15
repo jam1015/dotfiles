585697b63f3ba77e598cecd91a915689e5d007e2
jam1015 <jordan.mandel@live.com>
2022-03-15 14:35:35 -0400

    going back to linux

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -2,6 +2,7 @@
            (env . (("DOOMDIR" . "~/.doom.d")))))
 
 ("default" . ((user-emacs-directory . "~/.emacs.default")))
+
 ("evil" . ((user-emacs-directory . "~/.emacs.evil")))
 
  ("spacemacs" . ((user-emacs-directory . "~/spacemacs")
