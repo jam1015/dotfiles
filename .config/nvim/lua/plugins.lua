e142ab2c67c366a4540a6c760065adff0d4babd4
Jordan Mandel <jordan.mandel@live.com>
2022-06-05 21:02:18 -0400

    changed vimrc

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -169,7 +169,7 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-     disable = { "latex"},
+     disable = { "latex", "help"},
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
