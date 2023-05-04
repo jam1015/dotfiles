4cec70a24cdfd426c9cf31a177b30067252c406f
Jordan Mandel <jordan.mandel@live.com>
2023-05-04 10:54:51 -0400

    changed plugin loading

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,3 @@ require('keymaps') -- ~/.config/nvim/lua/keymaps/init.lua
 require('lazy_config') -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
 require('settings') -- ~/.config/nvim/lua/settings.lua
 
-
-
-
