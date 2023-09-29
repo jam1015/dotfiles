108291988e5d065b79215614b3b2a5f9c2486122
jam1015 <jordan.mandel@live.com>
2023-09-29 13:41:15 -0400

    removed s S x X from yankstack

--- .config/nvim/lua/plugin_configs/vim-yankstack/initi.lua
+++ .config/nvim/lua/plugin_configs/vim-yankstack/initi.lua
@@ -1,3 +1,5 @@
 vim.cmd([[
 let g:yankstack_map_keys = 0
+"let g:yankstack_yank_keys = ['c', 'C', 'd', 'D', 's', 'S', 'x', 'X', 'y', 'Y']
+let g:yankstack_yank_keys = ['c', 'C', 'd', 'D', 'y', 'Y']
 ]])
