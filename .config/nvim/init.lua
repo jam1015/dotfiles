f3ca05323a1171398aa9acf32e509da94caf0041
jam1015 <jordan.mandel@live.com>
2023-10-18 19:53:17 -0400

    made changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,35 @@
-require('autocommands') -- ~/.config/nvim/lua/autocommands.lua
-require('keymaps')      -- ~/.config/nvim/lua/keymaps/init.lua
-require('lazy_config')  -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
-						-- ~/.config/nvim/lua/plugin_keymaps.lua
-require('settings')     -- ~/.config/nvim/lua/settings.lua
-require('keymaps.post') -- ~/.config/nvim/lua/keymaps/post.lua
+-- First, ensure you have nvim-lspconfig installed and loaded
+
+local lspconfig = require('lspconfig')
+
+-- Helper function to search for a root marker file upwards in the directory tree
+local root_pattern = lspconfig.util.root_pattern
+
+-- Configuration for Lua (sumneko) language server
+lspconfig.lua_ls.setup({
+	cmd = {"/Users/jordanmandel/Documents/lua-language-server/bin/lua-language-server", "-E", "/Users/jordanmandel/Documents/lua-language-server/bin/main.lua"},
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+				path = vim.split(package.path, ';'),
+			},
+			diagnostics = {
+				enable = true,
+				globals = {'vim'},
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+				},
+			},
+		},
+	},
+	root_dir = function(filename)
+		return root_pattern(".git")(filename) or
+		root_pattern("init.lua", "main.lua", ".lua")(filename) or
+		lspconfig.util.path.dirname(filename)
+	end
+})
+
