2b8ae84562a530c814715cbc48be91435cf9c928
Jordan Mandel <jordan.mandel@live.com>
2023-11-05 14:21:54 -0500

    made changes

--- .config/nvim/lua/plugin_configs/vim-slime-ext-plugins/config.lua
+++ .config/nvim/lua/plugin_configs/vim-slime-ext-plugins/config.lua
@@ -1,6 +1,4 @@
 require("plugin_keymaps").pluginKeymaps("vim-slime")
-	vim.g.slime_target_send = "slime_neovim#send"
-	vim.g.slime_target_config = "slime_neovim#config"
 	vim.g.slime_input_pid=0
-	vim.g.ruled_terminal=1
-	vim.g.override_status=1
+	vim.g.ruled_terminal=true
+	vim.g.override_status=true
