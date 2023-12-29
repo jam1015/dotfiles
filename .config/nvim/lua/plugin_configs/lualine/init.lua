26c097071e99f14833055865eda38ff22554333e
Jordan Mandel <jordan.mandel@live.com>
2023-12-29 14:49:11 -0500

    configged lualine

--- .config/nvim/lua/plugin_configs/lualine/init.lua
+++ .config/nvim/lua/plugin_configs/lualine/init.lua
@@ -14,15 +14,50 @@ end
 
 
 
+
+local function git_root_dir_name()
+    -- Get the full path of the current file
+    local filepath = vim.fn.expand('%:p:h')
+    
+    -- Check if the filepath is valid
+    if filepath == "" or filepath == nil then
+        return ''
+    end
+
+    -- Construct and execute the git command
+    local git_cmd = "git -C " .. filepath .. " rev-parse --show-toplevel 2> /dev/null"
+    local f = io.popen(git_cmd)
+    
+    -- Check if the command executed successfully
+    if not f then
+        return ''
+    end
+
+    local git_root = f:read("*a")
+    f:close()
+
+    -- Check and format the result
+    if git_root and #git_root > 0 then
+        git_root = string.gsub(git_root, "\n$", "")  -- Trim the newline character
+        -- Extract only the base directory name
+        local base_dir_name = git_root:match("([^/\\]+)$")
+        return base_dir_name
+    else
+        return ''
+    end
+end
+
+
+
 require('lualine').setup {
 	options = {
 		icons_enabled = true,
 		theme = used_theme,
 		--'mytheme', --'auto',
-		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' },
-		--component_separators = { left = '', right = '' },
-		--section_separators = { left = '', right = '' },
+		--component_separators = { left = '', right = '' },
+		--section_separators = { left = '', right = '' },
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -39,7 +74,7 @@ require('lualine').setup {
 	sections = {
 		lualine_a = { '   ' },
 		lualine_b = { 'branch', 'diff', 'diagnostics' },
-		lualine_c = { '%f' },
+		lualine_c = { { git_root_dir_name, icon = '' }, '%f' },
 		lualine_x = { 'encoding', 'fileformat', 'filetype' },
 		lualine_y = { 'progress' },
 		lualine_z = { 'location', 'b:terminal_job_id', 'b:terminal_job_pid' }
@@ -52,12 +87,12 @@ require('lualine').setup {
 		lualine_x = { 'encoding', 'fileformat', 'filetype' },
 		lualine_y = { 'progress' },
 		lualine_z = { 'location', 'b:terminal_job_id', 'b:terminal_job_pid' }
---		lualine_a = {},
---		lualine_b = {},
---		lualine_c = { 'filename' },
---		lualine_x = { 'location' },
---		lualine_y = {},
---		lualine_z = { 'b:terminal_job_id', 'b:terminal_job_pid' }
+		--		lualine_a = {},
+		--		lualine_b = {},
+		--		lualine_c = { 'filename' },
+		--		lualine_x = { 'location' },
+		--		lualine_y = {},
+		--		lualine_z = { 'b:terminal_job_id', 'b:terminal_job_pid' }
 	},
 	tabline = {},
 	winbar = {},
