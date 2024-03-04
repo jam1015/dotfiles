975f121011998e669ef2d8456900c98731b8170e
Jordan Mandel <jordan.mandel@live.com>
2024-03-04 00:37:37 -0500

    added new files

---
+++ .config/nvim/lua/plugin_configs/lualine/config.lua
@@ -0,0 +1,127 @@
+local ok, my_custom_theme = pcall(require, 'plugin_configs.lualine.my_auto_theme')
+
+local used_theme = nil
+if ok then
+	used_theme = my_custom_theme('normal')
+	--vim.cmd([[colorscheme delek]])
+else
+	used_theme = 'auto'
+	--vim.cmd([[colorscheme blue]])
+end
+
+vim.cmd([[
+function! Get_chan_jobid() abort
+try
+	let out =&channel
+	catch
+	let out = ""
+endtry
+return out
+endfunction
+]])
+
+local function get_chan_jobid()
+	return vim.api.nvim_eval('&channel > 0 ? &channel : ""')
+end
+
+local function get_chan_jobpid()
+	local out = vim.api.nvim_exec2([[
+      let pid_out = ""
+
+      try
+        let pid_out = string(jobpid(&channel))
+        " in case an external process kills the terminal's shell
+      catch /^Vim\%((\a\+)\)\=:E900/
+      endtry
+			echo pid_out
+	]], {output = true})
+	return out["output"] --returns as string
+end
+
+
+
+
+local function getVcsIcon()
+	-- Check if 'vcs_icon' exists in the current buffer's namespace
+	if vim.b.vcs_icon ~= nil then
+		return vim.b.vcs_icon
+	else
+		-- Return an empty string if 'vcs_icon' does not exist
+		return ""
+	end
+end
+
+
+
+local function get_slime_jobid()
+  if vim.b.slime_config and vim.b.slime_config.jobid then
+    return vim.b.slime_config.jobid
+  else
+    return ""
+  end
+end
+
+local function get_slime_pid()
+  if vim.b.slime_config and vim.b.slime_config.pid then
+    return vim.b.slime_config.pid
+  else
+    return ""
+  end
+end
+
+
+
+
+
+
+
+require('lualine').setup {
+	options = {
+		icons_enabled = true,
+		theme = used_theme,
+		--'mytheme', --'auto',
+		--component_separators = { left = '', right = '' },
+		--section_separators = { left = '', right = '' },
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = {},
+		lualine_b = { { 'branch', padding = { left = 0, right = 0 } }, {'diff',padding = { left = 1, right = 0 }}, {'diagnostics',padding = { left = 1, right = 0 }}},
+		lualine_c = { { "b:vcs_icon", padding = { left = 1, right = 0 } }, { "b:vcs_base_dir" }, { '%f', padding = { left = 0, right = 0 } } },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location', get_chan_jobid , get_chan_jobpid, get_slime_jobid, get_slime_pid}
+	},
+	inactive_sections = {
+
+		lualine_a = {},
+		lualine_b = { { 'branch', padding = { left = 0, right = 0 } }, {'diff',padding = { left = 1, right = 0 }}, {'diagnostics',padding = { left = 1, right = 0 }}},
+		lualine_c = { { "b:vcs_icon", padding = { left = 1, right = 0 } }, { "b:vcs_base_dir" }, { '%f', padding = { left = 0, right = 0 } } },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location', get_chan_jobid , get_chan_jobpid, get_slime_jobid, get_slime_pid}
+		--		lualine_a = {},
+		--		lualine_b = {},
+		--		lualine_c = { 'filename' },
+		--		lualine_x = { 'location' },
+		--		lualine_y = {},
+		--		lualine_z = { 'b:terminal_job_id', 'b:terminal_job_pid' }
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+}
