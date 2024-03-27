8aba92699903ab83061f742506fbd74a45e4b354
Jordan Mandel <jordan.mandel@live.com>
2024-03-27 19:35:32 -0400

    added files

---
+++ .config/nvim/lua/plugin_configs/nvim_window.lua
@@ -0,0 +1,18 @@
+require('nvim-window').setup({
+  -- The characters available for hinting windows.
+  chars = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  },
+
+  -- A group to use for overwriting the Normal highlight group in the floating
+  -- window. This can be used to change the background color.
+  normal_hl = 'Normal',
+
+  -- The highlight group to apply to the line that contains the hint characters.
+  -- This is used to make them stand out more.
+  hint_hl = 'Bold',
+
+  -- The border style to use for the floating window.
+  border = 'single'
+})
