9a4dd005636abc2ede36146eb2660f0101303d7c
Jordan Mandel <jordan.mandel@live.com>
2024-03-27 20:09:08 -0400

    added numbers

--- .config/nvim/lua/plugin_configs/nvim_window.lua
+++ .config/nvim/lua/plugin_configs/nvim_window.lua
@@ -1,18 +1,18 @@
 require('nvim-window').setup({
-  -- The characters available for hinting windows.
-  chars = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-  },
+	-- The characters available for hinting windows.
+	chars = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+	},
 
-  -- A group to use for overwriting the Normal highlight group in the floating
-  -- window. This can be used to change the background color.
-  normal_hl = 'Normal',
+	-- A group to use for overwriting the Normal highlight group in the floating
+	-- window. This can be used to change the background color.
+	normal_hl = 'Normal',
 
-  -- The highlight group to apply to the line that contains the hint characters.
-  -- This is used to make them stand out more.
-  hint_hl = 'Bold',
+	-- The highlight group to apply to the line that contains the hint characters.
+	-- This is used to make them stand out more.
+	hint_hl = 'Bold',
 
-  -- The border style to use for the floating window.
-  border = 'single'
+	-- The border style to use for the floating window.
+	border = 'single'
 })
