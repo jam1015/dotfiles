5f06790611cc77475cb1eb70d8a2c9fc6ceac6f0
Jordan Mandel <jordan.mandel@live.com>
2024-06-05 16:03:37 -0400

    changed dots hooks

---
+++ .dots_hooks/config.bash
@@ -0,0 +1,12 @@
+
+# Enable or disable remote actions
+export DOTSREMOTEACTIONS=true
+
+# Enable or disable rebase attempts before merge
+export DOTSTRYREBASE=true
+
+# Rebase strategy: choose from "theirs", "ours", or leave empty for default
+export DOTSREBASESTRATEGY=""
+
+# whether to run
+export RUN=true
