b76b2795ba4b08b5357f67caea29aa71b58090b3
Jordan Mandel <jordan.mandel@live.com>
2024-06-08 20:17:27 -0400

    cleaned up emacs

--- .config/chemacs/profiles.el
+++ .config/chemacs/profiles.el
@@ -12,12 +12,10 @@
             )
   )
  ("default" . ((user-emacs-directory . "~/.emacs.default")))
- ("default_old" . ((user-emacs-directory . "~/.emacs.default_old")))
  ("vanilla" . ((user-emacs-directory . "~/.emacs.vanilla")))
  ("evil" . ((user-emacs-directory . "~/.emacs.evil")))
- ("meow" . ((user-emacs-directory . "~/.emacs.meow")))
  ("god" . ((user-emacs-directory . "~/.emacs.god")))
- )
+)
 
 
 
