79285d5461d46f9b4f5c08f99acc3ffcc6123aa9
Jordan Mandel <jordan.mandel@live.com>
2024-08-07 15:37:20 -0400

    deleted whitespace

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,4 +4,3 @@ require('lazy_config')  -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
                         -- ~/.config/nvim/lua/plugin_keymaps.lua
 require('settings')     -- ~/.config/nvim/lua/settings.lua
 require('keymaps.post') -- ~/.config/nvim/lua/keymaps/post.lua
-
