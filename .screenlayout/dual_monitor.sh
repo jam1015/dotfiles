8a3209dafca0e9a6f7e2e9188b2e0fca81153bec
Jordan Mandel <jordan.mandel@live.com>
2024-08-13 15:41:00 -0400

    added entry

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS1 --mode 1600x900 --pos 1920x180 --rotate normal --output VGA1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP --primary --mode 2256x1504 --pos 728x1080 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off --output DisplayPort-10 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-11 --off --output DisplayPort-12 --mode 1920x1080 --pos 0x0 --rotate normal
