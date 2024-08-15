786232806baec4a51b6840349f7726d8514c9862
Jordan Mandel <jordan.mandel@live.com>
2024-08-15 13:13:01 -0400

    made small changes

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 2256x1504 --pos 728x1080 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off --output DisplayPort-10 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-11 --off --output DisplayPort-12 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr   --output DisplayPort-10 --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-12 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP --same-as DisplayPort-0 \
+  --output DisplayPort-0 --primary --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off  --output DisplayPort-11 --off \
+#--primary --mode 2256x1504 --pos 728x1080 --rotate normal --output
