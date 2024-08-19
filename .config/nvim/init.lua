094496c79e2bc2b64a40d7956d6195f79687c1ca
Jordan Mandel <jordan.mandel@live.com>
2024-08-19 16:05:30 -0400

    added ascii

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
+--  _______  _|__| _____  
+-- /    \  \/ /  |/     \ 
+--|   |  \   /|  |  Y Y  \
+--|___|  /\_/ |__|__|_|  /
+--     \/              \/ 
 require('autocommands') -- ~/.config/nvim/lua/autocommands.lua
 require('keymaps')      -- ~/.config/nvim/lua/keymaps/init.lua
 require('lazy_config')  -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
