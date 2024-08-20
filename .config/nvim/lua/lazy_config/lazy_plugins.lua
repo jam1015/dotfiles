0adf36051c445848a687547a6234f3b2f34775f2
Jordan Mandel <jordan.mandel@live.com>
2024-08-20 13:21:44 -0400

    added colorscheme

--- .config/nvim/lua/lazy_config/lazy_plugins.lua
+++ .config/nvim/lua/lazy_config/lazy_plugins.lua
@@ -607,7 +607,15 @@ return {
 
 
 
-
+  ({
+    "yorik1984/newpaper.nvim",
+    priority = 1000,
+    config = function()
+      require("newpaper").setup({
+        style = "light"
+      })
+    end
+  }),
 
 
 
