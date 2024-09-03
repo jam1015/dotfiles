2e57577de08a4a11c433cb3ec37622db9daa7821
Jordan Mandel <jordan.mandel@live.com>
2024-09-03 15:09:03 -0400

    changed font, explicit resolution

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -5,7 +5,7 @@ external_displays=$(xrandr | grep 'DisplayPort' | grep ' connected' | cut -d' ' 
 
 if [ -z "$external_displays" ]; then
     # No external displays found, run xrandr --auto
-    xrandr --auto
+xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
 else
     # Configure displays as specified
     xrandr --output eDP --mode 1280x720 --pos 1273x1080 --rotate normal \
