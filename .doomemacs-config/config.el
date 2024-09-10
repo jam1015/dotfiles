ab2d19ef337596c7e3b5425a23ae2ef2aab16155
Jordan Mandel <jordan.mandel@live.com>
2024-09-10 18:39:19 -0400

    added restrictive quit to doom

--- .doomemacs-config/config.el
+++ .doomemacs-config/config.el
@@ -82,7 +82,33 @@
 
 (with-eval-after-load 'evil-maps
   (define-key evil-normal-state-map (kbd "C-a") 'evil-window-map)
-  (define-key evil-normal-state-map (kbd "C-w") nil))
+  (define-key evil-normal-state-map (kbd "C-w") nil)
+(evil-define-command evil-quit (&optional force)
+  "Close the current window, current frame, current tab, Emacs.
+If the current frame belongs to some client the client connection
+is closed."
+  :repeat nil
+  (interactive "<!>")
+  (condition-case nil
+      (delete-window)
+    (error
+     (if (and (bound-and-true-p server-buffer-clients)
+              (fboundp 'server-edit)
+              (fboundp 'server-buffer-done))
+         (if force
+             (server-buffer-done (current-buffer))
+           (server-edit))
+       (condition-case nil
+           (delete-frame)
+         (error
+          (condition-case nil
+              (tab-bar-close-tab)
+            (error
+             (if force (if force
+                 (kill-emacs)
+               (save-buffers-kill-emacs)))))))))))
+
+  )
 
 
 
