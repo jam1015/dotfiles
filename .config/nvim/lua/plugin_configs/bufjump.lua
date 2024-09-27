3acc9000dc20ceed3ab8d89e7ecc035ff90969b8
Jordan Mandel <jordan.mandel@live.com>
2024-09-27 14:09:51 -0400

    actually fixed bufjump

--- .config/nvim/lua/plugin_configs/bufjump.lua
+++ .config/nvim/lua/plugin_configs/bufjump.lua
@@ -1,7 +1,9 @@
 require("bufjump").setup({
-  forward = false,
-  backward = false,
-  on_success = nil
+  forward_key = false,
+  backward_key = false,
+  on_success = function()
+    vim.cmd([[execute "normal! g`\"zz"]])
+  end,
 })
 
 require("plugin_keymaps").bufjump()
