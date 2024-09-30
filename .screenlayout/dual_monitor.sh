a33156a98eb58626f9ca4b8a3a348e561f1c0104
Jordan Mandel <jordan.mandel@live.com>
2024-09-30 19:19:35 -0400

    fixed xorg config

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-
 # Check for connected external displays excluding the default eDP
 external_displays=$(xrandr | grep 'DisplayPort' | grep ' connected' | cut -d' ' -f1)
 
 if [ -z "$external_displays" ]; then
-  # No external displays found, run xrandr --auto
-  xrandr --auto
-  xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
-  xrandr --auto
+ #xrandr --auto
 else
-  # Configure displays as specified
   xrandr --output eDP --mode 1280x720 --pos 1273x1080 --rotate normal \
     --output DisplayPort-0 --off \
     --output DisplayPort-1 --off \
