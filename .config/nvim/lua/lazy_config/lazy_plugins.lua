69f2cd9bd9623b435efddd967740ecc194735354
Jordan Mandel <jordan.mandel@live.com>
2024-10-05 20:25:39 -0400

    added colorscheme

--- .config/nvim/lua/lazy_config/lazy_plugins.lua
+++ .config/nvim/lua/lazy_config/lazy_plugins.lua
@@ -13,6 +13,24 @@ return {
     --event = "VeryLazy",
     opts = require("plugin_configs.which-key")
   },
+  -- TODO: write move config to its own file
+  {
+    "ilof2/posterpole.nvim",
+    config = function()
+      require("posterpole").setup({
+        transparent = false,
+        colorless_bg = false,           -- grayscale or not
+        dim_inactive = false,           -- highlight inactive splits
+        brightness = 0,                 -- negative numbers - darker, positive - lighter
+        selected_tab_highlight = false, --highlight current selected tab
+        fg_saturation = 0,              -- font saturation, gray colors become more brighter
+        bg_saturation = 0               -- background saturation
+      })
+      vim.cmd("colorscheme posterpole")
+    end
+  },
+  {
+    'bettervim/yugen.nvim' },
   {
     'gcmt/vessel.nvim',
     config = function()
