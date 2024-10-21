f650163e45bb256dd8a27734e9af22c3b76b9700
Jordan Mandel <jordan.mandel@live.com>
2024-10-21 15:53:17 -0400

    changed theme

--- .emacs.default/init.el
+++ .emacs.default/init.el
@@ -48,17 +48,3 @@
 ;;
 ;;;;(load-theme 'nord t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wheatgrass))
- '(custom-safe-themes
-   '("4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
