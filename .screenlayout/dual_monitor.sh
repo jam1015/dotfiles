0f609623ab6166a86f19f98ef10d45129d01ed26
Jordan Mandel <jordan.mandel@live.com>
2024-10-31 10:27:04 -0400

    changed arrangement

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -6,5 +6,5 @@ if [ -z "$external_displays" ]; then
   echo "haha" > /tmp/xlog.txt
   #xrandr --auto
 else
- xrandr --output eDP --mode 1024x768 --pos 1123x1152 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off --output DisplayPort-10 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-11 --off --output DisplayPort-12 --primary --mode 1920x1080 --pos 1080x72 --rotate normal
+  xrandr --output eDP --mode 1024x768 --pos 1159x1265 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DisplayPort-9 --off --output DisplayPort-10 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-11 --off --output DisplayPort-12 --primary --mode 1920x1080 --pos 1080x185 --rotate normal
 fi
