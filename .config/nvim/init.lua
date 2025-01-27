d467f4bac22da9e31aefa10e033147f54aed3b04
Jordan Mandel <jordan.mandel@live.com>
2025-01-27 09:31:05 -0500

    changed nvim ascii font

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,12 @@
-
-
--- __ _  _  _  __  _  _ 
+-- __ _  _  _  __  _  _
 --(  ( \/ )( \(  )( \/ )
 --/    /\ \/ / )( / \/ \
 --\_)__) \__/ (__)\_)(_/
 
-
-require('autocommands') -- ~/.config/nvim/lua/autocommands.lua
-require('keymaps')      -- ~/.config/nvim/lua/keymaps/init.lua
-require('lazy_config')  -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
-                      -- ~/.config/nvim/lua/plugin_keymaps.lua
+require('autocommands')  -- ~/.config/nvim/lua/autocommands.lua
+require('keymaps')       -- ~/.config/nvim/lua/keymaps/init.lua
+require('lazy_config')   -- ~/.config/nvim/lua/lazy_config/lazy_plugins.lua
+                         -- ~/.config/nvim/lua/plugin_keymaps.lua
 require('settings')      -- ~/.config/nvim/lua/settings.lua
 require('keymaps.post')  -- ~/.config/nvim/lua/keymaps/post.lua
 
