c50664d43d1a9162021d7e4cfbef96823d0a3ca0
Jordan Mandel <jordan.mandel@live.com>
2025-03-21 12:39:36 -0400

    added term close autocommand

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -53,7 +53,7 @@ local my_term_autocmds = api.nvim_create_augroup("my_nvim_terminal", { clear = t
 
 local function no_term_num()
   local bufnr = vim.api.nvim_get_current_buf()
-  local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
+  local buftype = vim.api.nvim_get_option_value( 'buftype', {buf = bufnr})
   if buftype == 'terminal' then
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
@@ -64,6 +64,29 @@ end
 api.nvim_create_autocmd("TermOpen", {
   callback = no_term_num,
   group = my_term_autocmds,
+})
+
+local function close_after_term()
+    -- Defer a little to allow any new buffer to settle in
+    vim.defer_fn(function()
+      local buffers = vim.api.nvim_list_bufs()
+      local listed = {}
+      for _, buf in ipairs(buffers) do
+        if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_get_option_value("buflisted",{buf = buf}) then
+          table.insert(listed, buf)
+        end
+      end
+
+      -- If only one buffer is listed and it's unnamed, exit Vim
+      if #listed == 1 and vim.api.nvim_buf_get_name(listed[1]) == "" then
+        vim.cmd("qa!")
+      end
+    end, 100)
+  end
+
+vim.api.nvim_create_autocmd("TermClose", {
+  callback = close_after_term,
+  group = my_term_autocmds
 })
 
 
