7e158e6e371ea84ad1ed207a8e5abd0d17743576
Jordan Mandel <jordan.mandel@live.com>
2025-03-24 10:15:25 -0400

    snacks

---
+++ .config/nvim/lua/plugin_configs/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+  bigfile = { enabled = false },
+  dashboard = { enabled = false },
+  explorer = {},
+  indent = { enabled = false },
+  input = { enabled = false },
+  picker = {
+    sources = {
+      explorer = {
+        -- your explorer picker configuration comes here
+        -- or leave it empty to use the default settings
+      }
+    }
+
+  },
+  notifier = { enabled = false },
+  quickfile = { enabled = false },
+  scope = { enabled = false },
+  scroll = { enabled = false },
+  statuscolumn = { enabled = false },
+  words = { enabled = false },
+}
