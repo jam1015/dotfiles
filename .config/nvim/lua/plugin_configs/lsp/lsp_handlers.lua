f203d7247a7f9abc37b068969de1c169ec034814
Jordan Mandel <jordan.mandel@live.com>
2025-04-07 12:35:58 -0400

    wp

--- .config/nvim/lua/plugin_configs/lsp/lsp_handlers.lua
+++ .config/nvim/lua/plugin_configs/lsp/lsp_handlers.lua
@@ -116,16 +116,20 @@ M.setup = function() -- more setup things
     { name = "DiagnosticSignHint", text = "" },
     { name = "DiagnosticSignInfo", text = "" },
   }
+  local sign_config_table = {
+    { [vim.diagnostic.severity.ERROR] = "" },
+    { [vim.diagnostic.severity.WARN] = "" },
+    { [vim.diagnostic.severity.HINT] = "" },
+    { [vim.diagnostic.severity.INFO] = "" },
+  }
 
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
 
   local config = {
     -- disable virtual text
     virtual_text = false,
     -- show signs
     signs = {
+      text = sign_config_table,
       active = signs,
     },
     update_in_insert = true,
@@ -142,14 +146,6 @@ M.setup = function() -- more setup things
   }
 
   vim.diagnostic.config(config)
-
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    --		border = "rounded",
-  })
-
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    --		border = "rounded",
-  })
 end
 
 return M
