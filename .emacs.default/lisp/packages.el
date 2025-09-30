03185f23cfafbc17bb4f785dd07e91e544c35742
Jordan Mandel <jordan.mandel@live.com>
2025-09-30 17:01:21 -0400

    reordered emacs packages

--- .emacs.default/lisp/packages.el
+++ .emacs.default/lisp/packages.el
@@ -1,62 +1,58 @@
 (require 'elpaca-setup) ;; remember elpaca-after-init-hook  and :ensure (:build (+elpaca/build-if-new))
 ;;(load-relative "straight_setup.el")
 
-;;; code
-
-(require 'which-key-config)
+;;; evil
 (require 'evil-leader-config)
 (require 'evil-config)
-(require 'god-mode-config)
-(require 'geiser-mit-config)
-(require 'undo-tree-config)
-(require 'anzu-config)
 (require 'evil-anzu-config)
-(require 'evil-collection-config)
-(require 'sly-config)
-(require 'sly-quicklisp-config)
-(require 'sly-asdf-config)
-(require 'flycheck-config)
-(require 'package-lint-config)
-(require 'cursor-contraster-config)
+(require 'anzu-config)
 (require 'evil-god-toggle-config)
 (require 'evil-goggles-config)
-(require 'corfu-config)
-(require 'dabbrev-config)
-(require 'vertico-config)
-(require 'anisochromatic-theme-config)
-(require 'solarized-theme-config)
-(require 'markdown-mode-config)
-(require 'xclip-config)
+(require 'evil-collection-config)
+(require 'cursor-contraster-config)
+(require 'god-mode-config)
+(require 'evil-textobj-entire-config)
+
+;;; elisp
+(require 'flycheck-config)
+(require 'package-lint-config)
+(require 'elisp-lint-config)
+
+;;; completion
 (require 'orderless-config)
 (require 'marginalia-config)
 (require 'consult-config)
 (require 'embark-config)
 (require 'embark-consult-config)
-(require 'wgrep-config)
 (require 'cape-config)
+(require 'dabbrev-config)
+(require 'vertico-config)
+(require 'corfu-config)
+
+;;; lisp
+(require 'sly-config)
+(require 'sly-quicklisp-config)
+(require 'sly-asdf-config)
+(require 'geiser-mit-config)
+
+;;; convenience
+(require 'which-key-config)
+(require 'undo-tree-config)
+(require 'xclip-config)
+(require 'wgrep-config)
+
+;;; themes
+(require 'anisochromatic-theme-config)
+(require 'solarized-theme-config)
+
+
+;;; utilities
 (require 'dirvish-config)
-(require 'evil-textobj-entire-config)
-(require 'dashboard-config)
-(require 'elisp-lint-config)
 (require 'gnus-config)
+(require 'dashboard-config)
 
 
-
-
-
-
-
-
-;; use-package with Elpaca:
-
-
-
-
-
-
-
-
-
-
+;;; other filetypes
+(require 'markdown-mode-config)
 
 (provide 'packages)
