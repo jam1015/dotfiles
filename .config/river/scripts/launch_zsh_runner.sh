01e574aebc6ee5451334933f28091ca63b54eadf
Jordan Mandel <jordan.mandel@live.com>
2025-10-01 08:34:12 -0400

    zsh launcher fixed

--- .config/river/scripts/launch_zsh_runner.sh
+++ .config/river/scripts/launch_zsh_runner.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 riverctl enter-mode normal
+
+WHEREAMI=$(cat ~/.local/state/zsh/whereami)
 # Kill any existing launcher instances
 pkill -f "ZSH_LAUNCHER=1" 2>/dev/null
 
 # Launch terminal with the launcher environment variable
 # Using kitty since you have tiny_samedir_kitty in your config
-kitty --class=Launcher -e env ZSH_LAUNCHER=1 zsh -i
+/usr/bin/kitty  --directory="$WHEREAMI" --class=Launcher -e env ZSH_LAUNCHER=1 zsh -o vi -i
