fdd79e006665d7f4049ee1495bc3ac2f95754e0a
Jordan Mandel <jordan.mandel@live.com>
2026-04-13 11:13:36 -0400

    wip

--- .emacs.default/early-init.el
+++ .emacs.default/early-init.el
@@ -2,6 +2,12 @@
 (setq package-enable-at-startup nil)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-;;(add-to-list 'default-frame-alist '(font . "InputMonoNerdFont-10"))
+(defun my/setup-frame-font (frame)
+  (when (display-graphic-p frame)
+    (set-frame-font "Lilex Nerd Font Mono-10" t (list frame))))
+
+(add-hook 'after-make-frame-functions #'my/setup-frame-font)
+(when (display-graphic-p)
+  (set-frame-font "Lilex Nerd Font Mono-10" t t))
 (scroll-bar-mode -1)
 ;;;end early-init.el
